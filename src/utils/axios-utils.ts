b974f33132b9bf525232c0d1b556ab7ed068e03b
cks612 <kyusung612@gmail.com>
2022-10-14 12:46:54 +0900

    axios added

--- src/utils/axios-utils.ts
+++ src/utils/axios-utils.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
+import type { AxiosResponse } from "axios";
 
 const client = axios.create({
   baseURL: "http://localhost:3000",
 });
 
-export const request = ({ ...options }) => {
+export const request = async ({ ...options }) => {
   client.defaults.headers.common.Authorization = "Bearer token";
-  const onSuccess = (response: any) => response;
+  const onSuccess = (response: AxiosResponse) => response;
   const onError = (error: Error) => {
     return error;
   };
